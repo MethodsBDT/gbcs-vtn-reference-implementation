ad0903a6d7f51004f61f6dfd669545c71333dfa7
Grzegorz Bocian <grzegorz.bocian@codibly.com>
2023-11-02 10:51:34 +0100

    review: add review suggestions

--- swagger_server/controllers/auth_controller.py
+++ swagger_server/controllers/auth_controller.py
@@ -11,13 +11,13 @@ def fetch_token():  # noqa: E501
 
     :rtype: str
     """
-    client_id = connexion.request.headers["clientID"]
-    client_secret = connexion.request.headers["clientSecret"]
+    client_id = connexion.request.headers['clientID']
+    client_secret = connexion.request.headers['clientSecret']
 
-    logging.debug(f"fetch_token: client_id={client_id} client_secret={client_secret}")
+    logging.debug(f'fetch_token: client_id={client_id} client_secret={client_secret}')
 
     auth_provider = AuthServiceProvider()
     try:
         return auth_provider.get_token(client_id, client_secret)
     except IOError:
-        logging.error("Could not fetch token")
+        logging.error('Could not fetch token')
