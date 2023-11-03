4ae1e3ba67769dfff5ace415e35c89ea142ee6f7
Grzegorz Bocian <grzegorz.bocian@codibly.com>
2023-11-03 12:33:22 +0100

    feat: replace server to production ready

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -3,13 +3,16 @@
 import connexion
 
 from swagger_server import encoder
+from gevent.pywsgi import WSGIServer
 from config import SERVER_PORT
+
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'OpenADR REST Demand Response API'}, pythonic_params=True)
-    app.run(port=SERVER_PORT)
+    http_server = WSGIServer(('0.0.0.0', SERVER_PORT), app)
+    http_server.serve_forever()
 
 
 if __name__ == '__main__':
