2a883b7a0ec1f1ef7a274a26e607725f5518b173
oadr3 <openadr3@openadr.org>
2024-06-12 16:27:03 -0600

    update client credential flow

--- swagger_server/controllers/auth_controller.py
+++ swagger_server/controllers/auth_controller.py
@@ -1,23 +1,38 @@
 import connexion
+from http import HTTPStatus
 import logging
+import six
 
+from swagger_server.models.client_credential_request import ClientCredentialRequest  # noqa: E501
+from swagger_server.models.client_credential_response import ClientCredentialResponse  # noqa: E501
+from swagger_server.models.problem import Problem  # noqa: E501
+from swagger_server import util
 from swagger_server.services.auth.auth_provider import AuthServiceProvider
 
 
-def fetch_token():  # noqa: E501
+def fetch_token(body):  # noqa: E501
     """fetch a token
 
     Return an access token based on clientID and clientSecret. # noqa: E501
 
-    :rtype: str
-    """
-    client_id = connexion.request.headers['clientID']
-    client_secret = connexion.request.headers['clientSecret']
+    :param body:
+    :type body: dict | bytes
 
-    logging.debug(f'fetch_token: client_id={client_id} client_secret={client_secret}')
+    :rtype: ClientCredentialResponse
+    """
+    if connexion.request.is_json:
+        body = ClientCredentialRequest.from_dict(connexion.request.get_json())  # noqa: E501
+
+    client_id = body.client_id
+    client_secret = body.client_secret
+    logging.debug(f'fetch_token: body={client_id} client_secret={client_secret}')
 
     auth_provider = AuthServiceProvider()
     try:
-        return auth_provider.get_token(client_id, client_secret)
+        token =  auth_provider.get_token(client_id, client_secret)
+        response = ClientCredentialResponse(access_token=token, token_type="Bearer")
+        return response, HTTPStatus.OK
     except IOError:
-        logging.error('Could not fetch token')
+        problem = Problem(title="Could not fetch token", status=str(HTTPStatus.INTERNAL_SERVER_ERROR))
+        logging.warning(f"fetch_token(): problem={problem}")
+        return problem, HTTPStatus.INTERNAL_SERVER_ERROR
