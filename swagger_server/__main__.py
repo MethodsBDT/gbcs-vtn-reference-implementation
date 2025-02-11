062b4da397e6768ef51a0232419877ac75d791ca
Steven Kalt <kalt.steven@gmail.com>
2025-02-11 07:52:18 -0500

    chore: remove .servers field from swagger.yaml
    See https://github.com/oadr3-org/specification/pull/250

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -8,7 +8,9 @@ from config import SERVER_PORT
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('swagger.yaml', arguments={'title': 'OpenADR REST Demand Response API'}, pythonic_params=True)
+    app.add_api('swagger.yaml', base_path="/openadr3/3.1.0", arguments={'title': 'OpenADR REST Demand Response API'}, pythonic_params=True)
+    # Note that the OpenADR3 protocol can run on any path; we're choosing to run it
+    # on /openadr3/3.1.0.
     http_server = WSGIServer(('0.0.0.0', SERVER_PORT), app)
     http_server.serve_forever()
 
